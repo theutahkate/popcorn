f5dac1af58bfefba46cee061799cc10494e23f96
Utah Kate Newman <utah.kate.newman@gmail.com>
2018-09-05 12:21:32 -0700

    set up onload call, active class for btns, death to var

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
-(function() {
-	document.querySelector('.no-js').classList.remove('no-js');
-})
-
 const main = document.querySelector("#main"),
 			titleLinks = document.querySelectorAll(".article__title-link"),
-			modal = document.querySelector("#modal");
+			modal = document.querySelector("#modal"),
+			srcBtns = document.querySelectorAll(".source__btn");
 
-document.querySelector("#nytimes").addEventListener("click", nytApi);
-document.querySelector("#ew").addEventListener("click", newsApi);
-document.querySelector("#tvline").addEventListener("click", newsApi);
+document.querySelector("#tvline").addEventListener("click", function() {
+	setActiveBtn(this);
+	newsApi(this);
+});
+document.querySelector("#ew").addEventListener("click", function() {
+	setActiveBtn(this);
+	newsApi(this);
+});
+document.querySelector("#nytimes").addEventListener("click", function() {
+	setActiveBtn(this);
+	nytApi();
+});
 document.querySelector("#closeModal").addEventListener("click", function() {
 	modal.classList.add("hidden");
 })
 
+function setActiveBtn(ele) {
+	for (let i = 0; i < srcBtns.length; i++) {
+		srcBtns[i].classList.remove("active");
+	}
+	ele.classList.add("active");
+}
+
 function buildArticle(dataObj, num) {
-	var	article = document.createElement('article'),
+	let	article = document.createElement('article'),
 			articleInnards =
 				`<div class='img-container'>
 						<img src='${dataObj.imgUrl}' alt=''>
@@ -38,7 +51,7 @@ function buildArticle(dataObj, num) {
 }
 
 function nytApi() {
-	var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+	let url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
 	url += '?' + $.param({
 	  'api-key': "6d2a32f885d74b27a33c0761d0f3b0f0"
 	});
@@ -46,11 +59,11 @@ function nytApi() {
 	  url: url,
 	  method: 'GET',
 	}).done(function(result) {
-		var nytResults = result.results,
+		let nytResults = result.results,
 				count = 0,
 				nytArray = normalizeNytData(nytResults);
 		main.innerHTML = "";
-	  for (var i = 0; i < nytArray.length; i++) {
+	  for (let i = 0; i < nytArray.length; i++) {
 		  buildArticle(nytArray[i], count);
 		  count++;
 	  }
@@ -59,21 +72,22 @@ function nytApi() {
 	});
 }
 
-function newsApi() {
+function newsApi(ele) {
 	const baseUrl = 'https://newsapi.org/v2/everything?domains=',
 			key = '&apiKey=66127631ccb94c1bb2e2a789f1d5392b';
-	var	src = `${this.id}.com`
+	let	src = ele === window ? "ew.com" : `${ele.id}.com`,
 			url = baseUrl + src + key,
 			req = new Request(url);
+
 	fetch(req)
 		.then(function(response) {
 			return response.json();
 		})
 		.then(function(data) {
-			var count = 0,
+			let count = 0,
 					parsedData = normalizeNewsApiData(data.articles);
 			main.innerHTML = "";
-		  for (var i = 0; i < parsedData.length; i++) {
+		  for (let i = 0; i < parsedData.length; i++) {
 			  buildArticle(parsedData[i], count);
 			  count++;
 		  }
@@ -81,9 +95,9 @@ function newsApi() {
 }
 
 function normalizeNytData(obj) {
-	var normalData = [];
-	for (var i = 0; i < obj.length; i++) {
-		var articleObj = {};
+	let normalData = [];
+	for (let i = 0; i < obj.length; i++) {
+		let articleObj = {};
 		articleObj.author = obj[i].byline;
 		articleObj.headline = obj[i].headline;
 		articleObj.imgUrl = obj[i].multimedia.src;
@@ -95,9 +109,9 @@ function normalizeNytData(obj) {
 }
 
 function normalizeNewsApiData(obj) {
-	var normalData = [];
-	for (var i = 0; i < obj.length; i++) {
-		var articleObj = {};
+	let normalData = [];
+	for (let i = 0; i < obj.length; i++) {
+		let articleObj = {};
 		articleObj.author = obj[i].author;
 		articleObj.description = obj[i].description;
 		articleObj.headline = obj[i].title;
@@ -107,3 +121,9 @@ function normalizeNewsApiData(obj) {
 	}
 	return normalData
 }
+
+(function() {
+	document.querySelector('.no-js').classList.remove('no-js');
+	setActiveBtn(document.querySelector("#ew"));
+	newsApi(this);
+})();
